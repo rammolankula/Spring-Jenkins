e933d6b63bc30929a8514ed410de6c8167810761
rammolankula <ramu.molankula.poorna@gmail.com>
2025-10-06 23:26:11 +0200

    make pipeline build check01

--- src/test/java/ram/explore/SpringJenkinsApplicationTests.java
+++ src/test/java/ram/explore/SpringJenkinsApplicationTests.java
@@ -1,6 +1,8 @@
 package ram.explore;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,9 +10,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+    Logger logger= LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
 	@Test
 	void contextLoads() {
         System.out.println("Test Cases executing....");
+        logger.info("Test:::01");
         assertEquals(true,true);
 	}
 }
